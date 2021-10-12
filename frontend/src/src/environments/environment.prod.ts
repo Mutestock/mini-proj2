017de337fbec2415a90497f6f84699220d1cd336
apatheticfrog <apathetic_frog@protonmail.com>
2021-10-12 14:43:09 +0200

    deleted frontend. edited docker-compose accordingly

--- frontend/src/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-const serverIp: string = '159.65.54.148';
-
-export const environment = {
-  production: true,
-  grpcUrl: `http://${serverIp}:10050`,
-  teacherRestUrl: `http://${serverIp}:10060`,
-  gradesUrl: `http://${serverIp}:10020`
-};
--- frontend/src/src/environments/environment.staging.ts
+++
@@ -1,8 +0,0 @@
-const serverIp: string = 'localhost';
-
-export const environment = {
-  production: true,
-  grpcUrl: `http://${serverIp}:10050`,
-  teacherRestUrl: `http://${serverIp}:10060`,
-  gradesUrl: `http://${serverIp}:10020`
-};
