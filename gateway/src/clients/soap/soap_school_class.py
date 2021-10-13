c9dbadd6c3a3bf84a61441856e91b842f3a3aabc
sofushn <sofushn@hotmail.dk>
2021-10-13 23:18:27 +0200

    updated gateway soap client to match soap endpoints

--- gateway/src/clients/soap/soap_school_class.py
+++ gateway/src/clients/soap/soap_school_class.py
@@ -9,23 +9,18 @@ def _create_client():
     return zeep.Client(wsdl=_WSDL_URL)
 
 
-### Update below to school-class wsdl functions
-
 def create_school_class(new_school_class):
-    _create_client().service.AddSchoolClass(new_school_class)
-
+    _create_client().service.CreateNewClass(new_school_class)
 
 def read_school_class(id):
-    _create_client().service.GetSchoolClass(id)
-
-
-def update_school_class(id, new_school_class):
-    _create_client().service.UpdateSchoolClass(id, new_school_class)
-
-
-def delete_school_class(id):
-    _create_client().service.DeleteSchoolClass(id)
-
+    _create_client().service.GetClass(id)
 
 def read_list_school_class():
-    _create_client().service.GetAllSchoolClasses()
+    _create_client().service.GetAllClasses()
+
+def add_person_class(classId, personId):
+    _create_client().service.AddPersonClass(classId, personId)
+
+def add_people_class(classId, personIds):
+    _create_client().service.AddPeopleClass(classId, personIds)
+    
