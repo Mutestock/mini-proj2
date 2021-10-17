b4261d14acc4efe719e595edf566c9b384e3c003
apatheticfrog <apathetic_frog@protonmail.com>
2021-10-17 15:39:05 +0200

    ppl passed|(grpc+rest) -> bus -> gateway OK. Need tests.

---
+++ gateway/src/clients/rest/rest_bus.py
@@ -0,0 +1,27 @@
+import requests
+from utils.config import CONFIG
+import json as JSON
+
+_CLIENT_CONFIG: str = CONFIG["clients"]["bus"]["mini-proj"]
+_PREFIX: str = f"http://{_CLIENT_CONFIG['host']}:{_CLIENT_CONFIG['port']}/hybrid"
+_HEADERS = {"Content-Type": "application/json"}
+
+
+##### Post #####
+
+##### Get #####
+
+
+def person_read_list_passed():
+    try:
+        return JSON.dumps(requests.get(f"{_PREFIX}/passed").json())
+    except Exception as e:
+        print()
+
+
+
+##### Put #####
+
+
+##### Delete #####
+
--- gateway/src/server/routes/person_routes.py
+++ gateway/src/server/routes/person_routes.py
@@ -5,6 +5,7 @@ from clients.grpc.grpc_person import (
     read_person_list,
     update_person,
 )
+from clientsr.rest.rest_bus import person_read_list_passed
 from entities.person import NewPerson
 from flask import request
 
@@ -24,12 +25,11 @@ def person_route_read_list():
 
 def person_route_update(id):
     try:
-        update_exam(id, NewPerson.from_json(request.json))
+        update_person(id, NewPerson.from_json(request.json))
         return "200"
     except Exception as e:
         print(e)
         return "500"
-
 
 def person_route_delete(id):
     try:
@@ -43,10 +43,14 @@ def person_route_delete(id):
 def person_route_read(id):
     return read_person(id)
 
+def person_route_read_list_passed():
+    return person_read_list_passed()
+
 
 def collect_routes(app):
     app.add_url_rule("/person", view_func=person_route_create, methods=["POST"])
     app.add_url_rule("/person", view_func=person_route_read_list, methods=["GET"])
+    app.add_url_rule("/person/passed", view_func=person_route_read_list_passed, methods=["GET"])
     app.add_url_rule("/person/<int:id>", view_func=person_route_update, methods=["PUT"])
     app.add_url_rule(
         "/person/<int:id>", view_func=person_route_delete, methods=["DELETE"]
