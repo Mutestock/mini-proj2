fad2314046dbe9a464bf2fd64eef398bb527d7e9
apatheticfrog <apathetic_frog@protonmail.com>
2021-10-20 15:34:47 +0200

    some docker configurations mainly

--- bus/src/main.rs
+++ bus/src/main.rs
@@ -21,5 +21,6 @@ async fn main() {
 
     let routes = exam_routes.or(hybrid_routes);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
+
