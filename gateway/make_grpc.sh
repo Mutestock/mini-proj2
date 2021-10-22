7cbb0afa320b86abe4131a6c7b4d59f1f7490100
apatheticfrog <apathetic_frog@protonmail.com>
2021-10-22 16:50:55 +0200

    moving all gRPC logic to bus

--- gateway/make_grpc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Cleanup
-rm -rf ./src/logic/protogen
-mkdir ./src/logic/protogen
-
-python -m grpc_tools.protoc -I./proto --python_out=./src/logic/protogen --grpc_python_out=./src/logic/protogen ./proto/person.proto
-
-
