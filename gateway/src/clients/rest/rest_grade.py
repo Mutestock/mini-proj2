32458108d0e64a3e51d73a9e9db2174265990d39
= <doublenegative@protonmail.com>
2021-10-23 17:38:23 +0200

    gateway deno detached

--- gateway/src/clients/rest/rest_grade.py
+++ gateway/src/clients/rest/rest_grade.py
@@ -2,8 +2,8 @@ import requests
 from utils.config import CONFIG
 import json as JSON
 
-_CLIENT_CONFIG: str = CONFIG["clients"]["rest"]["mini-proj"]
-_PREFIX: str = f"http://{_CLIENT_CONFIG['host']}:{_CLIENT_CONFIG['port']}/grade"
+_CLIENT_CONFIG: str = CONFIG["clients"]["bus"]["mini-proj"]
+_PREFIX: str = f"http://{_CLIENT_CONFIG['host']}:{_CLIENT_CONFIG['port']}/api/grade"
 _HEADERS = {"Content-Type": "application/json"}
 
 
@@ -49,7 +49,7 @@ def grade_read_list_by_exam_id():
 def grade_update_by_person_id(id, grade):
     try:
         return requests.put(
-            f"{_PREFIX}/p-id={id}", data=JSON.dumps(grade.__dict__), headers=_HEADERS
+            f"{_PREFIX}/p-id/{id}", data=JSON.dumps(grade.__dict__), headers=_HEADERS
         ).text
     except Exception as e:
         print(e)
@@ -58,7 +58,7 @@ def grade_update_by_person_id(id, grade):
 def grade_update_by_exam_id(id, grade):
     try:
         return requests.put(
-            f"{_PREFIX}/e-id={id}", data=JSON.dumps(grade.__dict__), headers=_HEADERS
+            f"{_PREFIX}/e-id/{id}", data=JSON.dumps(grade.__dict__), headers=_HEADERS
         ).text
     except Exception as e:
         print(e)
@@ -69,13 +69,13 @@ def grade_update_by_exam_id(id, grade):
 
 def grade_delete_by_person_id(id):
     try:
-        return requests.delete(f"{_PREFIX}/p-id={id}").text
+        return requests.delete(f"{_PREFIX}/p-id/{id}").text
     except Exception as e:
         print(e)
 
 
 def grade_delete_by_exam_id(id):
     try:
-        return requests.delete(f"{_PREFIX}/e-id={id}").text
+        return requests.delete(f"{_PREFIX}/e-id/{id}").text
     except Exception as e:
         print(e)
